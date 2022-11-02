d108a71ba653c837e9af857ab48a469320233c14
LoveLess <116763405+LoveLess1212@users.noreply.github.com>
2022-11-02 08:28:47 +0100

    cc
    just add cc

--- Main.java
+++ Main.java
@@ -7,6 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String a = scanner.nextLine();
         if (Objects.equals(a, "OCT 31") || Objects.equals(a, "DEC 25")) System.out.print("yup");
-        else System.out.print("nope");
+        else System.out.print("nope"); // cc
     }
 }
